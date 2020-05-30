c9c7ac994188c4e39c571cd0d5a106a2e6909d30
foxe6-temp <foxe6@protonmail.com>
2020-05-31 01:09:18 +0800

    Automatic Deployment: Unknown Commitment

--- afbfff/core.py
+++ afbfff/core.py
@@ -4,18 +4,22 @@ import os
 
 
 class AFBFFF(object):
-    def __init__(self, item: str, zip: bool, mirror: bool = False, host: str = "AnonFiles",
-                 split: bool = True, split_size: int = 1024*1024*4000):
+    def __init__(self, item: str,
+                 split: bool = False, split_size: int = 1024*1024*4000,
+                 host: str = "AnonFiles", mirror: bool = False):
         if not os.path.isabs(item):
             item = join_path(abs_main_dir(3), item)
         if os.path.isfile(item):
-            if not zip:
-                if not split:
-                    if not mirror:
-                        globals()[host]().upload(item)
-                    else:
-                        AnonFiles().upload(item)
-                        BayFiles().upload(item)
-                        ForumFiles().upload(item)
+            if not split:
+                if not mirror:
+                    globals()[host]().upload(item)
+                else:
+                    AnonFiles().upload(item)
+                    BayFiles().upload(item)
+                    ForumFiles().upload(item)
+            else:
+                pass
+        elif os.path.isdir(item):
+            pass
 
 
