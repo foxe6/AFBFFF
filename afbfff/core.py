eb35ef86068026de067e24963c9f4df3c46d7c36
foxe6-temp <foxe6@protonmail.com>
2020-05-31 00:08:09 +0800

    Automatic Deployment: Unknown Commitment

--- afbfff/core.py
+++ afbfff/core.py
@@ -12,25 +12,24 @@ class AFBFFF(object):
         self.url += f"?token="+token if token else ""
 
     def upload(self, filename: str) -> dict:
+        issued = int(time.time())
+        response = None
         try:
-            issued = int(time.time())
-            response = None
             try:
                 response = requests.post(self.url, files={"file": open(filename, "rb")}).json()
                 return response
             except Exception as e:
                 raise e
+        finally:
             uploaded = int(time.time())
             sqlqueue = sqlq.SqlQueue(server=True, db="db.db", timeout_commit=100, depth=3)
             sql = '''CREATE TABLE IF NOT EXISTS "history" ("url" TEXT, "path" TEXT, "issued" INTEGER, "uploaded" INTEGER);'''
             sqlqueue.sql(sql)
             sql = '''INSERT INTO history VALUES (?, ?, ?, ?);'''
-            data = (response["data"]["file"]["url"]["short"], filename)
+            data = (response["data"]["file"]["url"]["short"], filename, issued, uploaded)
             sqlqueue.sql(sql, data)
             sqlqueue.commit()
             sqlqueue.stop()
-        except Exception as e:
-            raise e
 
 
 class AnonFiles(AFBFFF):
